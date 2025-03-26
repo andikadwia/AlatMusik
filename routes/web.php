7125e3e341502983d93d59e1794628965ab2eafb
Denyri <Denyriansyah24@gmail.com>
2025-03-26 13:05:10 +0700

    Menambahkan KontakController LandingpageController view dan menambahkan route di web.php

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,16 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ListItemController;
+use App\Http\Controllers\KontakController;
+use App\Http\Controllers\LandingpaController;
+use App\Http\Controllers\LandingpageController;
 
 Route::get('/register', [RegisterController::class, 'tampilkan']);
 Route::get('/login', [LoginController::class, 'tampilkan']);
 Route::get('/dashboard', [DashboardController::class, 'tampilkan']);
 Route::get('/listitem', [ListItemController::class, 'tampilkan']);
+Route::get('/landingpage', [LandingpageController::class, 'tampilkan']);
+Route::get('/Kontak', [KontakController::class, 'tampilkan']);
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
@@ -30,3 +35,11 @@ Route::get('/resteasy', function () {
 Route::get('/edit', function () {
     return view('edit');
 })->name('edit');
+
+Route::get('/kontak', function () {
+    return view('kontak');
+})->name('kontak');
+
+Route::get('/landingpage', function () {
+    return view('landingpage');
+})->name('landingpage');
